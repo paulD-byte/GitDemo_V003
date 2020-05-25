efdc81c56ffee1ccc32f3e86ccdd9e70c957fe81
paulD-byte <65722799+paulD-byte@users.noreply.github.com>
2020-05-25 12:31:49 +0530

    Third Commit......

---
+++ src/GitDemo3/Test1.java
@@ -0,0 +1,16 @@
+package GitDemo3;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry = "yes";
+
+		if (hungry == "yes") {
+			System.out.println("Eat Something......");
+		} else {
+			System.out.println("Do Work...........");
+		}
+	}
+}
